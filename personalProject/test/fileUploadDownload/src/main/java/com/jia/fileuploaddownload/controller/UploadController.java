06d906a1a048829e60abe4cf2ec87f7773675813
Baoshuai-Jia <jiabaoshuai@outlook.com>
2021-06-07 18:35:36 +0800

    0907

--- personalProject/test/fileUploadDownload/src/main/java/com/jia/fileuploaddownload/controller/UploadController.java
+++ personalProject/test/fileUploadDownload/src/main/java/com/jia/fileuploaddownload/controller/UploadController.java
@@ -1,15 +1,42 @@
 package com.jia.fileuploaddownload.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletResponse;
 
-@Controller
+
+@RestController
 public class UploadController {
 
     @RequestMapping("/upload")
     @ResponseBody
-    public String upload(){
+    public String upload(HttpServletResponse res){
+        res.setHeader("Access-Control-Allow-Origin", "*"); //最简单的设置跨域 *
+//        try {
+//            ServletOutputStream outputStream = res.getOutputStream();
+//            File file = new File("/Users/ebaoshuaijia/Downloads/cat (1).mp4");
+//            FileInputStream fileInputStream = new FileInputStream(file);
+//            IOUtils.copy(fileInputStream,outputStream);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//        }
         return "hello wendy";
     }
+
+
+    @RequestMapping("/test01")
+    @ResponseBody
+    public String test01(){
+        return "test01";
+    }
+
+
+
+
+
+
+
+
 }
